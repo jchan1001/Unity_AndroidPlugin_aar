c69b890a3667f25445b7b787a923de4b4e051df2
Chung Chanhoo <jchan1001@naver.com>
2018-10-05 00:12:22 +0900

    AppCompatActivity -> Activity Todo fix AppCompatActivity error

--- app/src/main/java/jchan1001/co/jp/unityplugin/Controller/UnityController.java
+++ app/src/main/java/jchan1001/co/jp/unityplugin/Controller/UnityController.java
@@ -1,6 +1,11 @@
 package jchan1001.co.jp.unityplugin.Controller;
 
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
+
+import jchan1001.co.jp.unityplugin.MainActivity;
+import jchan1001.co.jp.unityplugin.SecondActivity;
 
 import static android.util.Log.println;
 
@@ -19,6 +24,17 @@ public class UnityController {
         } else {
             return uc;
         }
+    }
+
+    //AndroidJavaException: java.lang.NoClassDefFoundError: Failed resolution of: Landroid/support/v7/app/AppCompatActivity;
+    //    java.lang.NoClassDefFoundError: Failed resolution of: Landroid/support/v7/app/AppCompatActivity;
+    public static void moveActivity(Context unityContext) {
+        Log.e("CCH", "OK MOVE0");
+//        Intent intent = new Intent(unityContext.getApplicationContext(), SecondActivity.class);
+        Intent intent = new Intent(unityContext.getApplicationContext(), MainActivity.class);
+        Log.e("CCH", "OK MOVE1");
+        unityContext.startActivity(intent);
+        Log.e("CCH", "OK MOVE2");
     }
 
     public void log(String tag, String msg) {
--- app/src/main/java/jchan1001/co/jp/unityplugin/MainActivity.java
+++ app/src/main/java/jchan1001/co/jp/unityplugin/MainActivity.java
@@ -1,13 +1,22 @@
 package jchan1001.co.jp.unityplugin;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+// Todo Fix AppCompatActivity Error
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static void log(String tag, String msg) {
+        Log.e(tag, msg);
     }
 }
