62d955823b0de26bc6557fc50f8500bb6883957b
Chung Chanhoo <jchan1001@naver.com>
2018-10-13 19:07:16 +0900

    Clean source

--- unityPlugin/src/main/java/jchan1001/co/jp/unityplugin/MainActivity.java
+++ unityPlugin/src/main/java/jchan1001/co/jp/unityplugin/MainActivity.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,9 +13,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import com.unity3d.player.UnityPlayer;
 
-// Todo Fix AppCompatActivity Error
-// AndroidJavaException: java.lang.NoClassDefFoundError: Failed resolution of: Landroid/support/v7/app/AppCompatActivity;
-// java.lang.NoClassDefFoundError: Failed resolution of: Landroid/support/v7/app/AppCompatActivity;
 public class MainActivity extends Activity {
 
     Button mCallUnityButtonChangeRed;
